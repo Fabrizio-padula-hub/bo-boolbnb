f11d1b22c056c16da68dfe29f5e82ba530ebb35a
Fabrizio-padula <fabriziopadula80@gmail.com>
2024-06-26 16:22:20 +0200

    feat: addition of the Dashboard layout and change of parameters

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -22,6 +22,7 @@
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
+                        {{-- Bottone log-out --}}
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
                             <div>{{ Auth::user()->name }}</div>
 
@@ -31,6 +32,7 @@
                                 </svg>
                             </div>
                         </button>
+                        {{-- Bottone log-out --}}
                     </x-slot>
 
                     <x-slot name="content">
