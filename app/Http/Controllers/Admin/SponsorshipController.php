b185d12aaa70cdba6462fb6a941f4fefc1c98829
Francesco-Rianna <maradonino98@hotmail.it>
2024-06-27 09:56:16 +0200

    Add data to Messages and Sponsor controllers and create corresponding Blade pages

--- app/Http/Controllers/Admin/SponsorshipController.php
+++ app/Http/Controllers/Admin/SponsorshipController.php
@@ -4,8 +4,15 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Sponsorship;
 
 class SponsorshipController extends Controller
 {
-    //
+    public function index()
+    {
+        
+        $sponsorships = Sponsorship::all(); 
+        // dd($sponshorships);
+        return view('admin.sponsorships', compact('sponsorships'));
+    }
 }
