d5cc57672c9752acde027b59c3b982534f4e8473
filippo-falcone <falconefilippo98@hotmail.com>
2024-06-27 11:46:51 +0000

    feat: change icon

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -18,17 +18,17 @@
                 <td class="py-3 px-2">{{ $apartment->visibility ? 'Si' : 'No' }}</td>
                 <td class="py-3 px-2">
                     <div class="inline-flex items-center space-x-3">
+                        <a href="{{ route('admin.apartments.show', $apartment->slug) }}" title="Mostra"
+                            class="hover:text-white"><svg class="w-5 h-5" viewBox="0 0 24 24" fill="none"
+                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+                                <circle cx="12" cy="12" r="3" />
+                            </svg>
+                        </a>
                         <a href="" title="Modifica" class="hover:text-white"><svg xmlns="http://www.w3.org/2000/svg"
                                 fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-                            </svg>
-                        </a>
-                        <a href="{{ route('admin.apartments.show', $apartment->slug) }}" title="Mostra"
-                            class="hover:text-white"><svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
                             </svg>
                         </a>
                         <a href="" title="Elimina" class="hover:text-white"><svg xmlns="http://www.w3.org/2000/svg"
