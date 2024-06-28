d3fc035e2f6394401aa1778e18cc024e0913b80f
Fabrizio-padula <fabriziopadula80@gmail.com>
2024-06-28 12:53:56 +0200

    feat: card implementation under 700px, button changes

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -1,17 +1,33 @@
 @extends('layouts.admin')
 @section('content')
     <table class="w-full whitespace-nowrap">
-        <thead class="bg-black/60">
+        <thead class="bg-black/60 max-md:hidden">
             <th class="text-left py-3 px-2 rounded-l-lg">{{ __('Titolo') }}</th>
             <th class="text-left py-3 px-2 max-md:hidden">{{ __('Indirizzo') }}</th>
             <th class="text-left py-3 px-2 max-md:hidden">{{ __('Visibilità') }}</th>
+            <th class="text-left py-3 px-2 max-md:hidden">{{ __('Immagine') }}</th>
             <th class="text-left py-3 px-2 rounded-r-lg">{{ __('Azioni') }}</th>
         </thead>
         @foreach ($apartments as $apartment)
-            <tr class="border-b border-gray-700">
+            <tr class="border-b border-gray-700 max-md:hidden">
                 <td class="py-3 px-2">{{ $apartment->title }}</td>
                 <td class="py-3 px-2 max-md:hidden">{{ $apartment->city }}</td>
                 <td class="py-3 px-2 max-md:hidden">{{ $apartment->visibility ? 'Si' : 'No' }}</td>
+                <td class="py-3 px-2 max-md:hidden">
+                    @if ($apartment->image)
+                        <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <rect x="3" y="3" width="18" height="18" rx="2" ry="2" />
+                            <circle cx="8.5" cy="8.5" r="1.5" />
+                            <polyline points="21 15 16 10 5 21" />
+                        </svg>
+                    @else
+                        <svg class="w-5 h-5 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    @endif
+                </td>
                 <td class="py-3 px-2">
                     <div class="inline-flex items-center space-x-3">
                         <a href="{{ route('admin.apartments.show', $apartment->slug) }}" title="Mostra"
@@ -21,8 +37,9 @@
                                 <circle cx="12" cy="12" r="3" />
                             </svg>
                         </a>
-                        <a href="{{ route('admin.apartments.edit', ['apartment' => $apartment->slug]) }}" title="Modifica" class="hover:text-white"><svg xmlns="http://www.w3.org/2000/svg"
-                                fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                        <a href="{{ route('admin.apartments.edit', ['apartment' => $apartment->slug]) }}" title="Modifica"
+                            class="hover:text-white"><svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                             </svg>
@@ -36,8 +53,44 @@
                     </div>
                 </td>
             </tr>
+
+            {{-- card per responsive sotto i 700px --}}
+            <div
+                class="max-w-sm mb-8 rounded-lg overflow-hidden shadow-lg md:hidden border-solid border-2 border-indigo-800">
+                <img class="w-full" src="https://v1.tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains">
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">{{ $apartment->title }}</div>
+                    <p class="text-gray-500 text-base">
+                        {{ $apartment->address }}
+                    </p>
+                </div>
+                {{-- bottoni azioni --}}
+                <div class="px-6 pt-4 pb-2">
+                    {{-- bottoni Mostra --}}
+                    <button type="submit"
+                        class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                        <a href="{{ route('admin.apartments.show', $apartment->slug) }}" title="Mostra"
+                            class="hover:text-white">Mostra
+                        </a>
+                    </button>
+                    {{-- bottoni Modifica --}}
+                    <button type="submit"
+                        class="ml-2 rounded-md bg-amber-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-amber-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                        <a href="{{ route('admin.apartments.edit', ['apartment' => $apartment->slug]) }}" title="Modifica"
+                            class="hover:text-white">
+                            Modifica
+                        </a>
+                    </button>
+                    {{-- bottoni Elimina --}}
+                    <button type="submit"
+                        class="ml-2 rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                        <a href="" title="Elimina" class="hover:text-white">
+                            Elimina
+                        </a>
+                    </button>
+                </div>
+            </div>
         @endforeach
-        
+
     </table>
-    
 @endsection
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -17,7 +17,7 @@
 <body>
     <div class="antialiased bg-black w-full min-h-screen text-slate-300 relative py-4">
         <div class="grid grid-cols-12 mx-auto gap-2 sm:gap-4 md:gap-6 lg:gap-10 xl:gap-14 max-w-7xl my-10 px-2">
-            <div id="menu" class="bg-white/10 col-span-3 rounded-lg p-4">
+            <div id="menu" class="bg-white/10 col-span-3 rounded-lg p-4 max-md:w-24">
                 <h1
                     class="font-bold text-lg lg:text-3xl max-md:hidden bg-gradient-to-br from-white via-white/50 to-transparent bg-clip-text text-transparent">
                     Dashboard
@@ -149,7 +149,7 @@
                 </div>
                 <p class="text-sm text-center max-md:hidden text-gray-600">🚀 &copy; 2024 Gruppo 5</p>
             </div>
-            <div id="content" class="bg-white/10 col-span-9 rounded-lg p-6 ">
+            <div id="content" class="bg-white/10 col-span-9 rounded-lg p-6">
                 @yield('content')
 
             </div>
