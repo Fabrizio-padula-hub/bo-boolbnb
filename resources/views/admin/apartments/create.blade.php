f80d554c1e7b609feeda161b14fad24b459ee0e3
WilliamHaxhija <williamhaxhija@gmail.com>
2024-06-29 15:51:07 +0200

    fix: aggiunta validazione per campo address

--- resources/views/admin/apartments/create.blade.php
+++ resources/views/admin/apartments/create.blade.php
@@ -5,7 +5,8 @@
         {{-- freccia per ritornare all'index --}}
         <div>
             <a href="{{ route('admin.apartments.index') }}">
-                <svg class="h-8 w-8 text-indigo-800 mb-4 hover:text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg class="h-8 w-8 text-indigo-800 mb-4 hover:text-white" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z" />
                 </svg>
@@ -61,6 +62,9 @@
                                             placeholder="Via Roma 13, 00118, Roma">
                                     </div>
                                 </div>
+                                @error('address')
+                                    <div class="alert alert-danger">{{ $message }}</div>
+                                @enderror
                             </div>
                             {{-- image --}}
                             <div class="col-span-full">
