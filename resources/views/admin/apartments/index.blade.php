65d88f1df39454e451b577abf71f9b74b0a23a14
filippo-falcone <falconefilippo98@hotmail.com>
2024-06-30 16:57:47 +0000

    fix: add underscore on index.blade

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -86,7 +86,7 @@
                         <button type="submit"
                             class="max-[457px]:mb-3 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                             <a href="{{ route('admin.apartments.show', $apartment->slug) }}" title="Mostra"
-                                class="hover:text-white">{{ _('Mostra') }}
+                                class="hover:text-white">{{ __('Mostra') }}
                             </a>
                         </button>
                         {{-- bottoni Modifica --}}
