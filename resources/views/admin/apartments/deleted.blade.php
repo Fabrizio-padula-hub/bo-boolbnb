973ba73a5ec22529fbe0e23efa9a0a329a6b3404
WilliamHaxhija <williamhaxhija@gmail.com>
2024-07-02 10:57:38 +0200

    fix: pop up icona force delete

--- resources/views/admin/apartments/deleted.blade.php
+++ resources/views/admin/apartments/deleted.blade.php
@@ -53,7 +53,7 @@
                     <td class="py-3 px-2">
                         {{-- icona Canella Definitivamente --}}
                         <a href="{{ route('admin.erased', ['apartment' => $apartment->slug]) }}"
-                            title="Ripristina" class="hover:text-red-400">
+                            title="Elimina definitivamente" class="hover:text-red-400">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                 stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                                 <path stroke-linecap="round" stroke-linejoin="round"
