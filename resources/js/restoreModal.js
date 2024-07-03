3568edeffb5b35d8c37e8ae4f67a1e56c719b6a0
filippo-falcone <falconefilippo98@hotmail.com>
2024-07-03 11:31:08 +0000

    fix: solve restored apartments error

--- resources/js/restoreModal.js
+++ resources/js/restoreModal.js
@@ -2,19 +2,24 @@ const allRestoreButtons = document.querySelectorAll('.ms-js-restore-btn');
 allRestoreButtons.forEach((restoreButton) => {
     restoreButton.addEventListener('click', function (event) {
         event.preventDefault();
-        const deleteModal = document.getElementById('ms-confirmRestoreModal');
+        const restoreModal = document.getElementById('ms-confirmRestoreModal');
+        const apartmentSlug = this.dataset.apartmentSlug;
         const apartmentTitle = this.dataset.apartmentTitle;
-        deleteModal.querySelector('.ms-modal-body').innerHTML = `Sei sicuro di voler ripristinare ${apartmentTitle}?`;
+        restoreModal.querySelector('.ms-modal-body').innerHTML = `Sei sicuro di voler ripristinare ${apartmentTitle}?`;
 
-        deleteModal.classList.remove('hidden');
-        deleteModal.classList.add('flex');
+        restoreModal.classList.remove('hidden');
+        restoreModal.classList.add('flex');
 
-
-        const modalCancelDeletionBtn = document.getElementById('ms-modal-cancel-restoration');
-        modalCancelDeletionBtn.addEventListener('click', function () {
+        const modalCancelRestorationBtn = document.getElementById('ms-modal-cancel-restoration');
+        modalCancelRestorationBtn.addEventListener('click', function () {
             event.preventDefault();
-            deleteModal.classList.remove('flex');
-            deleteModal.classList.add('hidden');
+            restoreModal.classList.remove('flex');
+            restoreModal.classList.add('hidden');
+        });
+
+        const modalConfiRestorationBtn = document.getElementById('ms-modal-confirm-restoration');
+        modalConfiRestorationBtn.addEventListener('click', function () {
+            this.setAttribute('href', `restore/${apartmentSlug}`)
         });
     });
 });
