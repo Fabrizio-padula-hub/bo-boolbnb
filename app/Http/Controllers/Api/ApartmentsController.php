d231caa77d875a2f74bbe8468681946621149ecb
filippo-falcone <falconefilippo98@hotmail.com>
2024-07-03 10:55:57 +0000

    feat: add api to ApartmentController

--- app/Http/Controllers/Api/ApartmentsController.php
+++ app/Http/Controllers/Api/ApartmentsController.php
@@ -4,16 +4,33 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Apartment;
+use Illuminate\Support\Facades\DB;
 
 class ApartmentsController extends Controller
 {
-    public function index() {
-        $apartments = Apartment::with('users', 'messages', 'visits', 'sponsorships', 'services')->get();
-        return response()->json([
-            'success' => true,
-            'results' => $apartments
-        ]);
-    }
+    public function index(Request $request)
+    {
+        try {
+            $latitude = $request->input('latitude');
+            $longitude = $request->input('longitude');
+            $radius = $request->input('radius');
 
+            $apartments = DB::table('apartments')
+                ->select(DB::raw('*, (6371 * acos(cos(radians(' . $latitude . ')) * cos(radians(lat)) * cos(radians(`long`) - radians(' . $longitude . ')) + sin(radians(' . $latitude . ')) * sin(radians(lat)))) AS distance'))
+                ->having('distance', '<', $radius)
+                ->orderBy('distance')
+                ->get();
+
+            $totalResults = $apartments->count();
+
+            return response()->json([
+                'total_results' => $totalResults,
+                'apartments' => $apartments
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => 'Si è verificato un errore durante l\'elaborazione della richiesta: ' . $e->getMessage()
+            ], 500);
+        }
+    }
 }
