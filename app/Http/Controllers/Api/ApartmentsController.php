ad5d408fbac58096942c74f1fd32387ba8d9494e
filippo-falcone <falconefilippo98@hotmail.com>
2024-07-04 23:21:32 +0000

    feat: add if for query services and if to filter of apartaments with service

--- app/Http/Controllers/Api/ApartmentsController.php
+++ app/Http/Controllers/Api/ApartmentsController.php
@@ -15,28 +15,45 @@ class ApartmentsController extends Controller
             $latitude = $request->input('latitude');
             $longitude = $request->input('longitude');
             $radius = $request->input('radius');
-            $serviceId = $request->input('service');
-            // Query per ottenere gli appartamenti con la distanza calcolata e i servizi correlati
-            $apartments = Apartment::select(
-                DB::raw('apartments.*, (6371 * acos(cos(radians(' . $latitude . ')) * cos(radians(apartments.lat)) * cos(radians(apartments.long) - radians(' . $longitude . ')) + sin(radians(' . $latitude . ')) * sin(radians(apartments.lat)))) AS distance')
+            $serviceIds = $request->input('services'); // Dal fo deve arrivare un array di id che sarà scritto nelle url in forma estare tipo &services[]=1&services[]=2
+
+            $query = Apartment::select(
+                'apartments.*',
+                DB::raw('(6371 * acos(cos(radians(' . $latitude . ')) * cos(radians(apartments.lat)) * cos(radians(apartments.long) - radians(' . $longitude . ')) + sin(radians(' . $latitude . ')) * sin(radians(apartments.lat)))) AS distance')
             )
-                // Join aggiustato per prendere tutti gli appartamenti che hanno un solo determinato servizio, aggiungere la possibilità di prenderle altri contemporaneamente
-                ->leftJoin('apartment_service', 'apartment_service.apartment_id', '=', 'apartments.id')
-                ->leftJoin('services', 'apartment_service.service_id', '=', 'services.id')
-                ->where('services.id', '=', $serviceId)
                 ->where('apartments.visibility', '=', 1)
-                // Per filtrale anche number of rooms, beds, bathrooms e square meters usare when
                 ->having('distance', '<', $radius)
                 ->orderBy('distance')
-                ->with('services') // Carica i servizi correlati
-                ->get();
+                ->with('services'); // Carica i servizi correlati
+
+            // Join aggiustato per prendere tutti gli appartamenti che hanno almeno un servizio selezionato compresa la possibilità di selezionarne più di uno
+            // (vi conviene aggiungere agli appartamenti nel db tramite edit tanti servizi così la potete testare bene)
+            // Con la if si rende il filtro opzionale solo se il 'array è popolato
+            if (!empty($serviceIds)) {
+                $query->leftJoin('apartment_service', 'apartment_service.apartment_id', '=', 'apartments.id')
+                    ->leftJoin('services', 'apartment_service.service_id', '=', 'services.id')
+                    ->whereIn('services.id', $serviceIds)
+                    ->groupBy('apartments.id');
+            }
+
+            // Per filtrale anche number of rooms, beds, bathrooms e square meters usare lo stesso metodo dei servizi con if (!empty) e dentro $query->where
+
+            $apartments = $query->get();
+
+            // Filtra gli appartamenti che hanno tutti i servizi richiesti, se forniti usando pluk per prendere gli id e pusharli in un array con toArray
+            if (!empty($serviceIds)) {
+                $apartments = $apartments->filter(function ($apartment) use ($serviceIds) {
+                    $apartmentServiceIds = $apartment->services->pluck('id')->toArray();
+                    return empty(array_diff($serviceIds, $apartmentServiceIds));
+                });
+            }
 
             // Conta il numero totale di risultati
             $totalResults = $apartments->count();
 
             return response()->json([
                 'total_results' => $totalResults,
-                'apartments' => $apartments
+                'apartments' => $apartments->values() // Ritorna i valori filtrati
             ]);
         } catch (\Exception $e) {
             return response()->json([
@@ -57,7 +74,7 @@ class ApartmentsController extends Controller
         } else {
             $data = [
                 'success' => false,
-                'error' => 'Non ci sono appartmenti che corrispondono a questo slug'
+                'error' => 'Non ci sono appartamenti che corrispondono a questo slug'
             ];
         }
         return response()->json($data);
