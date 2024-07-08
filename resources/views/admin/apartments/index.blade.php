232d86274c59ffb95808f1365b13d3457ff1f683
filippo-falcone <falconefilippo98@hotmail.com>
2024-07-08 16:22:15 +0000

    feat: add chart funcionality

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -49,16 +49,18 @@
                         {{-- icone --}}
                         <div class="inline-flex items-center space-x-3">
                             {{-- icona Sponsorizza --}}
-                            <a href="{{ route('admin.sponsorships.create', ['apartment' => $apartment->slug]) }}" title="Sponsorizza"
-                                class="hover:text-indigo-700"><svg class="w-5 h-5" viewBox="0 0 24 24" fill="none"
-                                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
-                                    <circle cx="12" cy="12" r="3" />
+                            <a href="{{ route('admin.sponsorships.create', ['apartment' => $apartment->slug]) }}"
+                                title="Sponsorizza" class="hover:text-indigo-700"><svg xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
+                                    <path d="M4.5 3.75a3 3 0 0 0-3 3v.75h21v-.75a3 3 0 0 0-3-3h-15Z" />
+                                    <path fill-rule="evenodd"
+                                        d="M22.5 9.75h-21v7.5a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3v-7.5Zm-18 3.75a.75.75 0 0 1 .75-.75h6a.75.75 0 0 1 0 1.5h-6a.75.75 0 0 1-.75-.75Zm.75 2.25a.75.75 0 0 0 0 1.5h3a.75.75 0 0 0 0-1.5h-3Z"
+                                        clip-rule="evenodd" />
                                 </svg>
                             </a>
                             {{-- icona Mostra --}}
                             <a href="{{ route('admin.apartments.show', $apartment->slug) }}" title="Mostra"
-                                class="hover:text-indigo-700"><svg class="w-5 h-5" viewBox="0 0 24 24" fill="none"
+                                class="hover:text-green-700"><svg class="w-5 h-5" viewBox="0 0 24 24" fill="none"
                                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                     <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
                                     <circle cx="12" cy="12" r="3" />
--- resources/views/admin/sponsorships/create.blade.php
+++ resources/views/admin/sponsorships/create.blade.php
@@ -1,90 +1,89 @@
 @extends('layouts.admin')
 
 @section('content')
-    <form id="payment-form" class="h-full" action="{{ route('admin.payment.checkout', ['apartment' => $apartment->slug]) }}"
-        method="POST" title="Sponsorizza">
-        @csrf
-        <div class="flex justify-between pb-3">
-            <h1>Sponsorizza l'appartamento '{{ $apartment->title }}'</h1>
-            <a id="ms-cartBtn" class="transition duration-150 ease-linear rounded-lg group">
-                <div class="relative pt-0.5 pr-1">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                        class="w-6 h-6 group-hover:text-indigo-400">
-                        <path
-                            d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
-                    </svg>
-
-                </div>
-            </a>
-        </div>
-        <div class="px-2 md-px-5 h-5/6">
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 h-full">
-                @foreach ($sponsorships as $sponsorship)
-                    <div
-                        class=" p-8 text-center rounded-3xl shadow-xl bg-clip-border border-solid border-2 border-indigo-800">
-                        <h1 class="text-indigo-400 font-semibold text-2xl">{{ $sponsorship->name }}</h1>
-                        <p class="pt-2 tracking-wide">
-                            <span class="align-top">€ </span>
-                            <span class="text-3xl font-semibold">{{ $sponsorship->price }}</span>
-                        </p>
-                        <hr class="mt-4 border-1">
-                        <div class="py-8">
-                            <p class="font-semibold text-gray-400 text-left flex">
-                                <span class="material-icons align-middle">
-                                    Durata
-                                </span>
-                                <span class="pl-2">
-                                    {{ $sponsorship->duration }}<span class="text-indigo-400">h</span>
-                                </span>
-                            </p>
-                            <p class="font-semibold text-gray-400 text-left pt-5">
-                                <span>
-                                    Con il pacchetto {{ $sponsorship->name }}, avrai la possibilità di mettere in evidenza
-                                    il
-                                    tuo
-                                    appartamento per
-                                    {{ $sponsorship->duration }}h
-                                </span>
-                            </p>
-                        </div>
-                        <button type="submit"
-                            class="max-[457px]:mb-3 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                            data-apartment-title="{{ $apartment->title }}">
-                            Sponsorizza
-                        </button>
-                    </div>
-                @endforeach
+    <div class="flex justify-between pb-3">
+        <h1>Sponsorizza l'appartamento '{{ $apartment->title }}'</h1>
+        <span id="ms-cartBtn" class="transition duration-150 ease-linear rounded-lg group cursor-pointer">
+            <div class="relative pt-0.5 pr-1">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                    class="w-6 h-6 group-hover:text-indigo-400">
+                    <path
+                        d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
+                </svg>
             </div>
+        </span>
+    </div>
+    <div class="px-2 md-px-5 h-5/6">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 h-full">
+            @foreach ($sponsorships as $sponsorship)
+                <div class="p-8 text-center rounded-3xl shadow-xl bg-clip-border border-solid border-2 border-indigo-800">
+                    <h1 class="text-indigo-400 font-semibold text-2xl">{{ $sponsorship->name }}</h1>
+                    <p class="pt-2 tracking-wide">
+                        <span class="align-top">€ </span>
+                        <span class="text-3xl font-semibold price">{{ $sponsorship->price }}</span>
+                    </p>
+                    <hr class="mt-4 border-1">
+                    <div class="py-8">
+                        <p class="font-semibold text-gray-400 text-left flex">
+                            <span class="material-icons align-middle">
+                                Durata
+                            </span>
+                            <span class="pl-2">
+                                {{ $sponsorship->duration }}<span class="text-indigo-400">h</span>
+                            </span>
+                        </p>
+                        <p class="font-semibold text-gray-400 text-left pt-5">
+                            <span>
+                                Con il pacchetto {{ $sponsorship->name }}, avrai la possibilità di mettere in evidenza
+                                il
+                                tuo
+                                appartamento per
+                                {{ $sponsorship->duration }}h
+                            </span>
+                        </p>
+                    </div>
+                    <div class="flex justify-center">
+                        <div class="flex items-center border-gray-100">
+                            <span
+                                class="decrement cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50">
+                                - </span>
+                            <span class="quantity h-8 w-8 border bg-white text-center text-xs outline-none">0</span>
+                            <span
+                                class="increment cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50">
+                                + </span>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
         </div>
-    </form>
-    {{-- <div id="dropin-container"></div> --}}
+    </div>
+
     {{-- Shopping cart --}}
-    <div id="ms-shoppingCart"
-        class="hidden mt-6 h-full border bg-white p-6 shadow-md md:mt-0 w-full md:w-1/3 lg:w-1/4 absolute right-0 top-0">
-        <span id="ms-closeCart">
+    <form id="payment-form"
+        class="ms-shoppingCart hidden h-full border bg-white p-6 shadow-md md:mt-0 w-full md:w-1/3 lg:w-1/4 absolute right-0 top-0"
+        action="{{ route('admin.payment.checkout', ['apartment' => $apartment->slug]) }}" method="POST"
+        title="Sponsorizza">
+        @csrf
+        <div id="ms-closeCart" class="cursor-pointer mb-4">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
                 <path fill-rule="evenodd"
-                    d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z"
+                    d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 1 1-1.06 1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z"
                     clip-rule="evenodd" />
             </svg>
-        </span>
-        <div class="mb-2 flex justify-between">
-            <p class="text-gray-700">Subtotal</p>
-            <p class="text-gray-700">$129.99</p>
         </div>
-        <div class="flex justify-between">
-            <p class="text-gray-700">Shipping</p>
-            <p class="text-gray-700">$4.99</p>
+        <div id="cartContainer">
+            <!-- Qui verranno aggiunti gli elementi del carrello -->
         </div>
         <hr class="my-4" />
         <div class="flex justify-between">
-            <p class="text-lg font-bold">Total</p>
-            <div class="">
-                <p class="mb-1 text-lg font-bold">$134.98 USD</p>
-                <p class="text-sm text-gray-700">including VAT</p>
+            <p class="text-lg font-bold">Totale</p>
+            <div>
+                <p class="mb-1 text-lg font-bold total">0.00€</p>
+                <p class="text-sm text-gray-700">inclusa IVA</p>
             </div>
         </div>
-        <button class="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600">Check
-            out</button>
-    </div>
+        {{-- <div id="dropin-container"></div> --}}
+        <button
+            class="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600">Acquista</button>
+    </form>
 @endsection
