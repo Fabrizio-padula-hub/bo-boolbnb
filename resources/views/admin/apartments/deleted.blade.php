f248102e13868b2b0eac8bb63503e145c564e8b4
WilliamHaxhija <williamhaxhija@gmail.com>
2024-07-10 11:56:18 +0200

    changed session messages style

--- resources/views/admin/apartments/deleted.blade.php
+++ resources/views/admin/apartments/deleted.blade.php
@@ -5,8 +5,20 @@
     </div>
     <div class="mb-4">
         @if (session()->has('message'))
-            <div class="text-green-600">
-                {{ session('message') }}
+            <div class="rounded text-green-900 px-4 py-3" role="alert">
+                <div class="flex">
+                    <div>
+                        <svg class="fill-current h-6 w-6 text-green-500 mr-4" xmlns="http://www.w3.org/2000/svg"
+                            xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img"
+                            preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+                            <path
+                                d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8m4.59-12.42L10 14.17l-2.59-2.58L6 13l4 4l8-8l-1.41-1.42z" />
+                        </svg>
+                    </div>
+                    <div>
+                        <p class="text-sm text-green-500">{{ session('message') }}</p>
+                    </div>
+                </div>
             </div>
         @endif
     </div>
