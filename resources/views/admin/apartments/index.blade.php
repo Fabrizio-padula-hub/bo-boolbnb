1908b527aac0c5b994578cd9be82d05ff58ecba6
filippo-falcone <falconefilippo98@hotmail.com>
2024-07-10 11:51:01 +0000

    feat: layout

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -17,7 +17,7 @@
         @endif
     </div>
     @if (count($apartments) !== 0)
-        <table class="w-full whitespace-nowrap max-md:hidden">
+        <table class="w-full whitespace-nowrap max-lg:hidden">
             <thead class="bg-black/90 max-md:hidden">
                 <th class="text-left py-3 px-2 rounded-l-lg">{{ __('Titolo') }}</th>
                 <th class="text-left py-3 px-2 max-md:hidden">{{ __('Indirizzo') }}</th>
@@ -25,7 +25,7 @@
                 <th class="text-left py-3 px-2 rounded-r-lg">{{ __('Azioni') }}</th>
             </thead>
             @foreach ($apartments as $apartment)
-                <tr class="border-b border-gray-700 max-md:hidden hover:bg-neutral-800">
+                <tr class="border-b border-gray-700 max-lg:hidden hover:bg-neutral-800">
                     <td class="py-3 px-2">{{ $apartment->title }}</td>
                     <td class="py-3 px-2 max-md:hidden">{{ $apartment->address }}</td>
                     <td class="py-3 px-2 max-md:hidden">{{ $apartment->visibility ? 'Si' : 'No' }}</td>
@@ -38,20 +38,25 @@
                             </a>
                             {{-- icona Mostra --}}
                             <a href="{{ route('admin.apartments.show', $apartment->slug) }}" title="Mostra"
-                                class="hover:text-green-700"><svg class="w-5 h-5" viewBox="0 0 24 24" fill="none"
-                                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
-                                    <circle cx="12" cy="12" r="3" />
+                                class="hover:text-green-700"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                                    fill="currentColor" class="w-5 h-5">
+                                    <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+                                    <path fill-rule="evenodd"
+                                        d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z"
+                                        clip-rule="evenodd" />
                                 </svg>
+
                             </a>
                             {{-- icona Modifica --}}
                             <a href="{{ route('admin.apartments.edit', ['apartment' => $apartment->slug]) }}"
                                 title="Modifica" class="hover:text-amber-400"><svg xmlns="http://www.w3.org/2000/svg"
-                                    fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                    class="w-5 h-5">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                    viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
+                                    <path
+                                        d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" />
+                                    <path
+                                        d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" />
                                 </svg>
+
                             </a>
                             {{-- icona Elimina --}}
                             <form action="{{ route('admin.apartments.destroy', ['apartment' => $apartment->slug]) }}"
@@ -72,62 +77,65 @@
                 </tr>
             @endforeach
         </table>
-        @foreach ($apartments as $apartment)
-            {{-- card per responsive sotto i 700px --}}
-            <div
-                class="max-w-sm mb-8 rounded-lg overflow-hidden shadow-lg md:hidden border-solid border-2 border-indigo-800">
-                @if ($apartment->image)
-                    <img class="w-full" src="{{ asset('storage/' . $apartment->image) }}" alt="{{ $apartment->title }}">
-                @endif
-                <div class="px-6 py-4">
-                    <div class="font-bold text-xl mb-2">{{ $apartment->title }}</div>
-                    <p class="text-gray-500 text-base">
-                        {{ $apartment->address }}
-                    </p>
-                    <p class="text-gray-500 text-base">
-                        {{ $apartment->visibility ? 'Visibile' : 'Non visibile' }}
-                    </p>
-                </div>
-                {{-- bottoni azioni --}}
-                <div class="px-6 pt-4 pb-2 grid grid-cols-2 gap-1">
-                    {{-- bottoni Sponsorizza --}}
-                    <button type="submit"
-                        class="max-[457px]:mb-3 rounded-md bg-indigo-600 px-3 py-2 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                        <a href="{{ route('admin.sponsorships.create', ['apartment' => $apartment->slug]) }}"
-                            title="Sponsorizza" class="hover:text-white">{{ __('Sponsorizza') }}
-                        </a>
-                    </button>
-                    {{-- bottoni Mostra --}}
-                    <button type="submit"
-                        class="max-[457px]:mb-3 rounded-md bg-green-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                        <a href="{{ route('admin.apartments.show', $apartment->slug) }}" title="Mostra"
-                            class="hover:text-white">{{ __('Mostra') }}
-                        </a>
-                    </button>
-                    {{-- bottoni Modifica --}}
-                    <button type="submit"
-                        class="max-[457px]:mb-3 rounded-md bg-amber-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-amber-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                        <a href="{{ route('admin.apartments.edit', ['apartment' => $apartment->slug]) }}" title="Modifica"
-                            class="hover:text-white">
-                            {{ __('Modifica') }}
-                        </a>
-                    </button>
-                    {{-- bottone Elimina --}}
-                    <form action="{{ route('admin.apartments.destroy', ['apartment' => $apartment->slug]) }}"
-                        method="POST" title="Elimina" class=" inline-block hover:text-red-800">
-                        @csrf
-                        @method('DELETE')
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-3">
+            @foreach ($apartments as $apartment)
+                {{-- card per responsive sotto i 700px --}}
+                <div
+                    class="w-full flex flex-col justify-between rounded-lg overflow-hidden shadow-lg lg:hidden border-solid border-2 border-indigo-800">
+                    @if ($apartment->image)
+                        <img class="w-full" src="{{ asset('storage/' . $apartment->image) }}"
+                            alt="{{ $apartment->title }}">
+                    @endif
+                    <div class="px-6 py-4">
+                        <div class="font-bold text-xl mb-2">{{ $apartment->title }}</div>
+                        <p class="text-gray-500 text-base">
+                            {{ $apartment->address }}
+                        </p>
+                        <p class="text-gray-500 text-base">
+                            {{ $apartment->visibility ? 'Visibile' : 'Non visibile' }}
+                        </p>
+                    </div>
+                    {{-- bottoni azioni --}}
+                    <div class="px-6 pt-4 pb-2 grid grid-cols-2 gap-1">
+                        {{-- bottoni Sponsorizza --}}
                         <button type="submit"
-                            class="ms-js-delete-btn w-full rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                            data-apartment-title="{{ $apartment->title }}">
-                            <a href="" title="Elimina" class="hover:text-white">
-                                {{ __('Elimina') }}
+                            class="max-[457px]:mb-3 rounded-md bg-indigo-600 px-3 py-2 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                            <a href="{{ route('admin.sponsorships.create', ['apartment' => $apartment->slug]) }}"
+                                title="Sponsorizza" class="hover:text-white">{{ __('Sponsorizza') }}
                             </a>
                         </button>
-                    </form>
+                        {{-- bottoni Mostra --}}
+                        <button type="submit"
+                            class="max-[457px]:mb-3 rounded-md bg-green-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                            <a href="{{ route('admin.apartments.show', $apartment->slug) }}" title="Mostra"
+                                class="hover:text-white">{{ __('Mostra') }}
+                            </a>
+                        </button>
+                        {{-- bottoni Modifica --}}
+                        <button type="submit"
+                            class="max-[457px]:mb-3 rounded-md bg-amber-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-amber-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                            <a href="{{ route('admin.apartments.edit', ['apartment' => $apartment->slug]) }}"
+                                title="Modifica" class="hover:text-white">
+                                {{ __('Modifica') }}
+                            </a>
+                        </button>
+                        {{-- bottone Elimina --}}
+                        <form action="{{ route('admin.apartments.destroy', ['apartment' => $apartment->slug]) }}"
+                            method="POST" title="Elimina" class=" inline-block hover:text-red-800">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit"
+                                class="ms-js-delete-btn w-full rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                data-apartment-title="{{ $apartment->title }}">
+                                <a href="" title="Elimina" class="hover:text-white">
+                                    {{ __('Elimina') }}
+                                </a>
+                            </button>
+                        </form>
+                    </div>
                 </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
     @else
         <p class="text-base font-semibold leading-7 py-2">In questa pagina trovi l'elenco completo di tutti
             gli appartamenti. Se vuoi aggiungere un appartamento nel nostro sistema lo troverai qui.</p>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -193,17 +193,13 @@
                                             <span class="text-white text-xs font-black">{{ $trashCount }}</span>
                                         </div>
                                     @endif
-                                    <svg width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
-                                        stroke="currentColor" fill="none" stroke-linecap="round"
-                                        stroke-linejoin="round"
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
                                         class="{{ Route::currentRouteName() === 'admin.deleted' ? 'w-8 h-8 text-indigo-400' : 'w-8 h-8 group-hover:text-indigo-400' }}">
-                                        <path stroke="none" d="M0 0h24v24H0z" />
-                                        <line x1="4" y1="7" x2="20" y2="7" />
-                                        <line x1="10" y1="11" x2="10" y2="17" />
-                                        <line x1="14" y1="11" x2="14" y2="17" />
-                                        <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
-                                        <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                                        <path fill-rule="evenodd"
+                                            d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z"
+                                            clip-rule="evenodd" />
                                     </svg>
+
                                 </div>
                             </div>
                             <div>
